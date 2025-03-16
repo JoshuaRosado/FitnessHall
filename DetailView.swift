771309f3ba0a6792268c5f30fa5bd061d3184855
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-16 14:44:36 -0500

    DetailView header upated

--- DetailView.swift
+++ DetailView.swift
@@ -18,10 +18,35 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView{
-            ZStack{
-                Image(workout.muscle)
-                    .resizable()
-                    .scaledToFit()
+            VStack{
+                ZStack(alignment: .bottomTrailing){
+                    ZStack{
+                        Rectangle()
+                            .fill(.space.opacity(0.8))
+                            .background(.thinMaterial)
+                            .shadow(color: .black.opacity(0.2), radius: 2, x: 3, y: 3)
+                        
+                        Text(workout.name)
+                            .font(.system(size: 40))
+                            .fontDesign(.rounded)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                    }
+                    .frame(height: 150)
+                    
+                    Text(workout.muscle)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .background(.ultraThinMaterial)
+                        .clipShape(.capsule)
+                        
+                        
+                    
+                }
+                    
+                    
+                
+                
             }
         }
         
